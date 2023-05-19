08e30142216afd887ddf389c93feea826002a026
AshiqAR <ashiqkallara.cet@gmail.com>
2023-05-19 09:52:01 +0530

    initial Commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function displayRoleDropdown() {
+    document.getElementById("roleDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('#signInOrSignUp')) {
+        var dropdowns = document.getElementsByClassName("dropdown");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+function clicked(role){
+    if(role == 'Home'){
+        console.log("home");
+        window.location.href = 'home.html';
+    }
+    else {
+        if(role == 'Patient'){
+            console.log('patient');
+            window.location.href = 'loginScreen.html';
+        }
+    }
+}
